b94d6587b8cdb2a9fe31a06c5f66a8e8ece3698b
elijahjohnson5553@gmail.com <elijahjohnson5553@gmail.com>
2020-01-26 21:54:30 -0800

    Merging Some Stuff

--- example/App.js
+++ example/App.js
@@ -12,7 +12,7 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import EAzureBlobStorage from 'react-native-azure-blob-storage';
 
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
